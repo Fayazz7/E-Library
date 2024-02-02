5a88d2381e54031589885f0ac078bcb757406eee
Fayaz Faizal <fayazblogger@gmail.com>
2024-02-03 01:16:10 +0530

    Remove all files from repository

--- README.md
+++
@@ -1,2 +0,0 @@
-# E-Library
-A Python-Django Web Application 
--- user/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from user import views
-
-urlpatterns = [
-    path('signup',views.SignUpView.as_view(),name='sign-up'),
-    path('home/',views.UserBookListView.as_view(),name='user-bk-home')
-]
